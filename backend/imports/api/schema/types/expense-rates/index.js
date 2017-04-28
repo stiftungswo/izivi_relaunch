602d6e5d045e4669fe5a95c2fa42b9971d33fa8e
Pascal Kaufmann <kaufmae@me.com>
2017-04-28 14:56:09 +0200

    upfront design of mission/specification graph

---
+++ backend/imports/api/schema/types/expense-rates/index.js
@@ -0,0 +1,13 @@
+import ExpenseRate from './_interface.graphql';
+import Catering from './catering.graphql';
+import Commute from './commute.graphql';
+import PocketMoney from './pocketMoney.graphql';
+import WorkingClothes from './workingClothes.graphql';
+// ...............
+export default [
+  ExpenseRate,
+  Catering,
+  Commute,
+  PocketMoney,
+  WorkingClothes,
+];
